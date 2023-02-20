720de43092d5d79531100ad4c196e89269ecd13a
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-20 15:25:40 +0100

    Add function that calculates using passed operator

--- main.js
+++ main.js
@@ -11,6 +11,22 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) return 'error';
+  if (b === 0) return 'just no';
   return a / b;
+}
+
+function operate(operator, a, b) {
+  // Calculates using the given operator
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return 'error';
+  }
 }
